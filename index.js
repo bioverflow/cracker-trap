4a0c12c8f5aff60932776e25f69cea5a66900fd2
Cami Rodriguez <camiepisode@outlook.com>
2018-01-29 17:04:36 -0500

    validating eslint

--- index.js
+++ index.js
@@ -1,74 +1,70 @@
-/*!
-	CrackerTrap based on devtools-detect
-	Detect if web developer tools is open to hardening apps
-	https://github.com/bioverflow/CrackerTrap
-	by Sindre Sorhus
-	MIT License
+/* eslint-disable no-debugger */
+/* eslint-disable spaced-comment */
+/*
+ * CrackerTrap based on devtools-detect
+ * Detect if web developer tools is open to hardening apps
+ * https://github.com/bioverflow/CrackerTrap
+ * by Sindre Sorhus
+ * MIT License
 */
-(function () {
-	'use strict';
+((function () {
+  const devtools = {
+    open: false,
+    orientation: null,
+    undocked: null,
+  };
+  const threshold = 160;
 
-	var devtools = {
-		open: false,
-		orientation: null,
-		undocked: null
-	};
-	var threshold = 160;
+  // Emit an event when devtools status is changed
+  const emitEvent = function emitEvent(state, orientation, undocked) {
+    window.dispatchEvent(new CustomEvent('onDevToolsChange', {
+      detail: {
+        open: state,
+        orientation,
+        undocked,
+      },
+    }));
+  };
 
-	/// Emit an event when devtools status is changed
-	var emitEvent = function (state, orientation, undocked) {
-		window.dispatchEvent(new CustomEvent('onDevToolsChange', {
-			detail: {
-				open: state,
-				orientation: orientation,
-				undocked: undocked
-			}
-		}));
-	};
+  function timeValidation() {
+    const startTime = new Date();
+    debugger;
+    const endTime = new Date();
 
-	function timeValidation() {
-		var startTime = new Date();
-		debugger;
-		var endTime = new Date();
+    return endTime - startTime > 100;
+  }
 
-		return endTime - startTime > 100;
-	}
+  // Every half second check if developer tools is opened or not
+  setInterval(() => {
+    const widthThreshold = window.outerWidth - window.innerWidth > threshold;
+    const heightThreshold = window.outerHeight - window.innerHeight > threshold;
+    const orientation = widthThreshold ? 'vertical' : 'horizontal';
 
-	/// Every half second check if developer tools is opened or not
-	setInterval(function () {
-		var widthThreshold = window.outerWidth - window.innerWidth > threshold;
-		var heightThreshold = window.outerHeight - window.innerHeight > threshold;
-		var orientation = widthThreshold ? 'vertical' : 'horizontal';
+    if (heightThreshold === true || widthThreshold === true) {
+      if (devtools.open === true || devtools.orientation !== orientation) {
+        emitEvent(true, orientation, null);
+      }
 
-		if (heightThreshold === true || widthThreshold === true) {
-			if (devtools.open === true || devtools.orientation !== orientation) {
-				emitEvent(true, orientation, null);
-			}
+      devtools.open = true;
+      devtools.orientation = orientation;
+      devtools.undocked = false;
+    } else if (timeValidation() === true) {
+      emitEvent(true, null, true);
+      devtools.undocked = true;
+    } else {
+      if (devtools.open) {
+        emitEvent(false, null, null);
+      }
 
-			devtools.open = true;
-			devtools.orientation = orientation;
-			devtools.undocked = false;
-		}
-		else {
-			if (timeValidation() == true) {
-				emitEvent(true, null, true);
-				devtools.undocked = true;
-			}
-			else {
-				if (devtools.open) {
-					emitEvent(false, null, null);
-				}
+      devtools.open = false;
+      devtools.orientation = null;
+      devtools.undocked = null;
+    }
+  }, 500);
 
-				devtools.open = false;
-				devtools.orientation = null;
-				devtools.undocked = null;
-			}
-		}
-	}, 500);
-
-	if (typeof module !== 'undefined' && module.exports) {
-		module.exports = devtools;
-	} else {
-		window.devtools = devtools;
-	}
-})();
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = devtools;
+  } else {
+    window.devtools = devtools;
+  }
+})());
