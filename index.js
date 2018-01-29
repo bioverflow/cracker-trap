83e54fa71f6a1303329a09bde54b0469ccdcc7e4
Cami Rodriguez <camiepisode@outlook.com>
2018-01-29 17:13:14 -0500

    removed eslint

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-debugger */
-/* eslint-disable spaced-comment */
 /*
  * CrackerTrap based on devtools-detect
  * Detect if web developer tools is open to hardening apps
@@ -8,63 +6,72 @@
  * MIT License
 */
 ((function () {
-  const devtools = {
-    open: false,
-    orientation: null,
-    undocked: null,
-  };
-  const threshold = 160;
+	let _settings = {
+		showAlert: false,
+		redirectToURI: false,
+		uri: '',
+	};
+	const devtools = {
+		open: false,
+		orientation: null,
+		undocked: null,
+	};
+	const threshold = 160;
 
-  // Emit an event when devtools status is changed
-  const emitEvent = function emitEvent(state, orientation, undocked) {
-    window.dispatchEvent(new CustomEvent('onDevToolsChange', {
-      detail: {
-        open: state,
-        orientation,
-        undocked,
-      },
-    }));
-  };
+	// Emit an event when devtools status is changed
+	const emitEvent = function emitEvent(state, orientation, undocked) {
+		window.dispatchEvent(new CustomEvent('onDevToolsChange', {
+			detail: {
+				open: state,
+				orientation,
+				undocked,
+			},
+		}));
+	};
 
-  function timeValidation() {
-    const startTime = new Date();
-    debugger;
-    const endTime = new Date();
+	function changeSettings(settings) {
+		_settings = settings;
+	}
 
-    return endTime - startTime > 100;
-  }
+	function timeValidation() {
+		const startTime = new Date();
+		debugger;
+		const endTime = new Date();
 
-  // Every half second check if developer tools is opened or not
-  setInterval(() => {
-    const widthThreshold = window.outerWidth - window.innerWidth > threshold;
-    const heightThreshold = window.outerHeight - window.innerHeight > threshold;
-    const orientation = widthThreshold ? 'vertical' : 'horizontal';
+		return endTime - startTime > 100;
+	}
 
-    if (heightThreshold === true || widthThreshold === true) {
-      if (devtools.open === true || devtools.orientation !== orientation) {
-        emitEvent(true, orientation, null);
-      }
+	// Every half second check if developer tools is opened or not
+	setInterval(() => {
+		const widthThreshold = window.outerWidth - window.innerWidth > threshold;
+		const heightThreshold = window.outerHeight - window.innerHeight > threshold;
+		const orientation = widthThreshold ? 'vertical' : 'horizontal';
 
-      devtools.open = true;
-      devtools.orientation = orientation;
-      devtools.undocked = false;
-    } else if (timeValidation() === true) {
-      emitEvent(true, null, true);
-      devtools.undocked = true;
-    } else {
-      if (devtools.open) {
-        emitEvent(false, null, null);
-      }
+		if (heightThreshold === true || widthThreshold === true) {
+			if (devtools.open === true || devtools.orientation !== orientation) {
+				emitEvent(true, orientation, null);
+			}
 
-      devtools.open = false;
-      devtools.orientation = null;
-      devtools.undocked = null;
-    }
-  }, 500);
+			devtools.open = true;
+			devtools.orientation = orientation;
+			devtools.undocked = false;
+		} else if (timeValidation() === true) {
+			emitEvent(true, null, true);
+			devtools.undocked = true;
+		} else {
+			if (devtools.open) {
+				emitEvent(false, null, null);
+			}
 
-  if (typeof module !== 'undefined' && module.exports) {
-    module.exports = devtools;
-  } else {
-    window.devtools = devtools;
-  }
+			devtools.open = false;
+			devtools.orientation = null;
+			devtools.undocked = null;
+		}
+	}, 500);
+
+	if (typeof module !== 'undefined' && module.exports) {
+		module.exports = devtools;
+	} else {
+		window.devtools = devtools;
+	}
 })());
